65b7d822932c828588658d5f4f9f7028e5cfc736
LAPTOP-PSEPRFOJ\Максим <budgo@mail.ru>
2021-08-03 17:14:13 +0300

    Добавлен минимап и начало сохранения

---
+++ Roll_a_Ball_Budgo/Assets/Scripts/MVC/Saved/IData.cs
@@ -0,0 +1,8 @@
+﻿namespace Assets.Scripts.MVC.Saved
+{
+    public interface IData<T>
+    {
+        void Save(T data, string path = null);
+        T Load(string path = null);
+    }
+}
---
+++ Roll_a_Ball_Budgo/Assets/Scripts/MVC/Saved/ISaveDataRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Assets.Scripts.MVC.Saved
+{
+    public interface ISaveDataRepository
+    {
+        void Save(PlayerBase player);
+        void Load(PlayerBase player);
+    }
+}
