960a86470d8cd8c86853ce37ec698f80e0ec05a7
Budgo <Budgo@mail.ru>
2021-08-05 12:24:42 +0300

    no message

--- Roll_a_Ball_Budgo/Assets/dataSave/data.bat
+++ Roll_a_Ball_Budgo/Assets/dataSave/data.bat
@@ -1,8 +1,8 @@
 <Player>
   <Name value="Roman" />
-  <PosX value="11,52982" />
+  <PosX value="11,41267" />
   <PosY value="6" />
-  <PosZ value="11,03749" />
+  <PosZ value="-6,783853" />
   <IsEnable value="True" />
   <Info Unity="2019.4.21f1">System Language: Russian</Info>
 </Player>
